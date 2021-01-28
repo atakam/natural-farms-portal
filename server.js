98736cd15269480d376dad67dea82d163497b67c
Austin Takam <austin.takam@ibm.com>
2021-01-28 09:42:27 -0500

    link client to server

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const app = require('./app/index');
+const path = require('path');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("running server");
