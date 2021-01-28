36abaa3ad8eb8934ef5b73c67c24998cb0e235bb
Austin Takam <austin.takam@ibm.com>
2021-01-28 12:06:15 -0500

    database pool update

--- databasePool.js
+++ databasePool.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 
+// const db = mysql.createConnection({
+//     host     : '162.241.224.239',
+//     user     : 'sihoneco_nfarms',
+//     password : 'LvdXfmECPs%;',
+//     database : 'sihoneco_naturalfarms'
+// });
+
 const db = mysql.createConnection({
-    host     : '162.241.224.239',
-    user     : 'sihoneco_nfarms',
-    password : 'LvdXfmECPs%;',
-    database : 'sihoneco_naturalfarms'
+    host     : 'us-cdbr-east-03.cleardb.com',
+    user     : 'bd429b0803fc4b',
+    password : '952fa139',
+    database : 'heroku_23c8218b3af09e3'
 });
 
 module.exports = db;
