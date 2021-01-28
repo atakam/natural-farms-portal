3a064b2aca390e6df325f0ac5c9a317d55a04f69
Austin Takam <austin.takam@ibm.com>
2021-01-28 09:14:23 -0500

    revert heroku commit

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 
-const path = require('path');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -18,10 +17,14 @@ const {
   ordersByUserRouter
 } = require('./api/orders');
 
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(express.static(buildPath));
-app.use(cors());
-
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- server/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app/index');
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
